c62e7a096f9e8a22bac9c75f30a43d8ba7854ccb
Maria Sophia M. Solo <solomariasophia@gmail.com>
2024-01-26 16:57:35 +0800

    fixing mistakes

--- sum_of_current_and_previous.py
+++ sum_of_current_and_previous.py
@@ -5,14 +5,14 @@ print ("Printing current and previous number sum in range (10)")
 previous_number = 0
 
 # repeat the first 10 numbers
-for i in range (1,5):
+for i in range (1,11):
 
 # then calculate for the sum of the current and previous number
     current_number = i
     sum = previous_number + current_number
 
 # print 
-    print("Sum of", current_number, "and", previous_number, "is", sum)
+    print("The previous number is", previous_number, "and", current_number, "sum:", sum)
 
 # update the previous number for the next repeatition 
 previous_number = current_number
